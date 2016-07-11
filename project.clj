496c73086759c1d161d61742b56fdfe19aba1eba
universsky <universsky@163.com>
2016-07-12 00:14:38 +0800

    Clojure Web 开发之 HelloWorld

--- project.clj
+++ project.clj
@@ -3,9 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main org.lightsword.course.db
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :main org.lightsword.clj_web
+  :dependencies [
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.5"]
                  [org.clojure/java.jdbc "0.3.6"]
                  [mysql/mysql-connector-java "5.1.25"]
                  [korma "0.3.0"]
+                 [selmer "0.7.2"]
+                 [ring "1.3.1"]
+                 [ring/ring-json "0.3.1"]
+                 [compojure "1.2.1"]
                  ])
--- src/org/lightsword/clj_web.clj
+++ src/org/lightsword/clj_web.clj
@@ -1,6 +1,20 @@
-(ns org.lightsword.clj_web)
+;; hello world
+(ns org.lightsword.clj_web
+  (:require [ring.adapter.jetty :as jetty]))
+
+(defn handler [request] {
+                        :status 200,
+                        :headers {"Content-Type" "text/html"}
+                        :body (str request "\n" "<h1>Hello, World!</h1>")
+                        })
+
+(defn start-server []
+  (jetty/run-jetty handler {
+                            :host "localhost"
+                            :port 3000
+                            }))
+
+
 
 (defn -main [& args]
-  (prn "Hello, World!"))
-
-(println "Hello,Clojure!")
+  (start-server))
